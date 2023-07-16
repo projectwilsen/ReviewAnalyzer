082a555c090e8bf74f5d4150246d239a13de2d83
unknown <projectwilsen@gmail.com>
2023-07-16 16:13:41 +0700

    add

--- core/ypa/models.py
+++ core/ypa/models.py
@@ -18,7 +18,10 @@ class Result(models.Model):
     positive_comment = models.CharField(max_length=5000, null=True, blank=True)
     total_negative_comment = models.CharField(max_length=10, null=True, blank=True)
     negative_comment = models.CharField(max_length=5000, null=True, blank=True)
+    total_neutral_comment = models.CharField(max_length=10, null=True, blank=True)
+    neutral_comment = models.CharField(max_length=5000, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.videoid
